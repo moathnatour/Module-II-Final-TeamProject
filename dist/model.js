34433c81046b1d01d1e49adfa179053e984e8fcb
Mental-potato <153oorrii153@gmail.com>
2025-03-02 20:25:39 +0200

    start of ball class

--- dist/model.js
+++ dist/model.js
@@ -1,1 +1,18 @@
-alert("model");
+var Ball = /** @class */ (function () {
+    function Ball(x, y) {
+        if (x === void 0) { x = 0; }
+        if (y === void 0) { y = 0; }
+        this.speed = 2;
+        this.radius = "med";
+        this.exist = true;
+        this.startX = x;
+        this.startY = y;
+        this.start();
+    }
+    return Ball;
+}());
+var player = /** @class */ (function () {
+    function player() {
+    }
+    return player;
+}());
--- model.ts
+++ model.ts
@@ -1,1 +1,52 @@
-alert("model");
+export class Ball{
+    speed : number = 2;
+    radius : string = "med";
+    exist : boolean = true;
+
+    color : string = "white";
+
+    startX : number;
+    startY : number;
+
+    dX : number;
+    dY: number;
+
+    constructor (public x = 0,public y = 0){
+        this.startX = x;
+        this.startY = y;
+
+        this.start();
+    }
+
+    isExist (){
+        return this.exist;
+    }
+
+    draw ()
+    {
+
+    }
+    getPositon(){
+
+    }
+    update(){
+        let boardX = //canvas width
+        let boardY = //canvas hight
+        this.x += this.dX * this.speed;
+        this.y += this.dY * this.speed;
+
+        if(this.x >= boardX || this.x <= 0){
+            this.exist = false;
+            this.dX = 0;
+            this.dY = 0;
+        }
+
+        if(this.y >= boardY || this.y <= 0){
+            this.dY = this.dY * -1;
+        }
+        
+    }
+
+class player{
+    
+}
