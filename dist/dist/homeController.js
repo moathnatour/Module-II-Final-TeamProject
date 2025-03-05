4442d454f315e24063e1dc5789a64159381e2295
moathnatour <moathnatour6@gmail.com>
2025-03-05 16:41:54 +0200

    creating toasts

---
+++ dist/dist/homeController.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.init = void 0;
+function init(singlePlayerButton, multliPlayerButton) {
+    function toggleGameOptionsToast() {
+    }
+}
+exports.init = init;
